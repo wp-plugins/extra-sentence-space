c3ccbe9eede422f07bc03b7391ec51d6bd0c164d
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-22 03:50:11 +0000

    * Update to v1.2 * Add filter 'c2c_extra_sentence_space_punctuation' to allow customization of the punctuation after which double-spacing (when present) is preserved. Default is '.!?' * Add filter 'c2c_extra_sentence_space' to respond to the function of the same name so that users can use the apply_filters('c2c_extra_sentence_space') notation for invoking function * Wrap function in if (function_exists()) check * Note compatibility with WP 3.0+ * Minor code reformatting (spacing) * Remove docs from top of plugin file (all that and more are in readme.txt) * Remove trailing whitespace in header docs * Add Filters and Upgrade Notice sections to readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/extra-sentence-space/tags/1.2@279221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extra-sentence-space.php
+++ extra-sentence-space.php
@@ -2,44 +2,30 @@
 /**
  * @package Extra_Sentence_Space
  * @author Scott Reilly
- * @version 1.1
+ * @version 1.2
  */
 /*
 Plugin Name: Extra Sentence Space
-Version: 1.1
-Plugin URI: http://coffee2code.com/wp-plugins/extra-sentence-space
+Version: 1.2
+Plugin URI: http://coffee2code.com/wp-plugins/extra-sentence-space/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Force browsers to display two spaces (when present) between sentences.
 
-Even though you may add two spaces after each sentence when writing a post (assuming you subscribe to a writing style 
-that suggests such spacing) web browsers will collapse consecutive blank spaces into a single space when viewed.
-This plugin adds a &nbsp; (non-breaking space) after sentence-ending punctuation to retain the appearance of your
-two-space intent.
+Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
 
-NOTE: The plugin will only enforce the two-space gap in places where two or more spaces actually separate sentences
-in your posts.  It will NOT insert a second space if only one space is present.
-
-Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/extra-sentence-space.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Begin (or continue) to use two spaces to separate your sentences when writing a post.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/extra-sentence-space/
 
 */
 
 /*
 Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -50,6 +36,7 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+if ( !function_exists( 'c2c_extra_sentence_space' ) ) :
 /**
  * Preserves two spaces (when present) between sentences for display as HTML.
  *
@@ -59,13 +46,16 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * @return string The converted text
  */
 function c2c_extra_sentence_space( $text ) {
-	return preg_replace("/([\.\!\?][\'\"]?)([ ]{2,})/imsU", "$1&nbsp; ", $text);
-} // end c2c_extra_sentence_space()
+	$punctuation = preg_quote( apply_filters( 'c2c_extra_sentence_space_punctuation', '.!?' ) );
+	return preg_replace( "/([$punctuation][\'\"]?)([ ]{2,})/imsU", "$1&nbsp; ", $text );
+}
+add_filter( 'c2c_extra_sentence_space', 'c2c_extra_sentence_space' );
+endif;
 
-add_filter('comment_text', 'c2c_extra_sentence_space', 9);
-add_filter('the_title', 'c2c_extra_sentence_space', 9);
-add_filter('the_content', 'c2c_extra_sentence_space', 9);
-add_filter('the_excerpt', 'c2c_extra_sentence_space', 9);
-add_filter('widget_text', 'c2c_extra_sentence_space', 9);
+add_filter( 'comment_text', 'c2c_extra_sentence_space', 9 );
+add_filter( 'the_title',    'c2c_extra_sentence_space', 9 );
+add_filter( 'the_content',  'c2c_extra_sentence_space', 9 );
+add_filter( 'the_excerpt',  'c2c_extra_sentence_space', 9 );
+add_filter( 'widget_text',  'c2c_extra_sentence_space', 9 );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -3,11 +3,12 @@ Contributors: coffee2code
 Donate link: http://coffee2code.com/donate
 Tags: formatting, post, content, space, coffee2code
 Requires at least: 1.5
-Tested up to: 2.9.1
-Stable tag: 1.1
-Version: 1.1
+Tested up to: 3.0.1
+Stable tag: 1.2
+Version: 1.2
 
 Force browsers to display two spaces (when present) between sentences.
+
 
 == Description ==
 
@@ -17,11 +18,13 @@ Even though you may add two spaces after each sentence when writing a post (assu
 
 NOTE: The plugin will only enforce the two-space gap in places where two or more spaces actually separate sentences in your posts.  It will NOT insert a second space if only one space is present.
 
+
 == Installation ==
 
 1. Unzip `extra-sentence-space.zip` inside the `/wp-content/plugins/` directory (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
 1. Begin (or continue) to use two spaces to separate your sentences when writing a post.
+
 
 == Frequently Asked Questions ==
 
@@ -33,10 +36,60 @@ This plugin potentially modifies the post content, excerpt, title, comment text,
 
 Did you use two spaces to separate the sentences when you wrote the post?  This plugin only retains the appearance of those two spaces when the post is viewed in a browser; it does not insert a second space if there wasn't one originally present.
 
+= Can I enforce double-spacing after other types of punctuation? =
+
+Yes.  See the Filters section for an example of the code you'll need to use.
+
+
+== Filters ==
+
+The plugin is further customizable via two filters. Typically, these customizations would be put into your active theme's functions.php file, or used by another plugin.
+
+= c2c_extra_sentence_space =
+
+The 'c2c_extra_sentence_space' filter allows you to use an alternative approach to safely invoke `c2c_extra_sentence_space()` in such a way that if the plugin were deactivated or deleted, then your calls to the function won't cause errors in your site.  This only applies if you use the function directly, which is not typical usage for most users.
+
+Example:
+
+Instead of:
+
+    `<?php echo c2c_extra_sentence_space( $mytext ); ?>`
+
+Do:
+
+    `<?php echo apply_filters( 'c2c_extra_sentence_space', $mytext ); ?>`
+
+= c2c_extra_sentence_space_punctuation =
+
+The 'c2c_extra_sentence_space_punctuation' filter allows you to customize the punctuation, characters, and/or symbols after which double-spacing (when present) is preserved.  By default these are '.!?'.
+
+Arguments:
+
+* $punctuation (string): The default characters after which double-spacing should be preserved. Default is '.!?'.
+
+Example:
+
+`add_filter( 'c2c_extra_sentence_space_punctuation', 'more_extra_space_punctuation' );
+function more_extra_space_punctuation( $punctuation ) {
+	// Adding the '/' and ')' characters to the list of characters
+	return $punctuation . '/)';
+}`
+
+
 == Changelog ==
 
+= 1.2 =
+* Add filter 'c2c_extra_sentence_space_punctuation' to allow customization of the punctuation after which double-spacing (when present) is preserved. Default is '.!?'
+* Add filter 'c2c_extra_sentence_space' to respond to the function of the same name so that users can use the apply_filters('c2c_extra_sentence_space') notation for invoking function
+* Wrap function in if (function_exists()) check
+* Note compatibility with WP 3.0+
+* Minor code reformatting (spacing)
+* Remove docs from top of plugin file (all that and more are in readme.txt)
+* Remove trailing whitespace in header docs
+* Add Filters and Upgrade Notice sections to readme.txt
+
 = 1.1 =
-* Create filter 'hide_broken_shortcode' to allow customization of the output for broken shortcodes
+* Also filter 'widget_text'
 * Now also filter widget_text
 * Add PHPDoc documentation
 * Note compatibility with WP 2.8+ and 2.9+
@@ -51,3 +104,9 @@ Did you use two spaces to separate the sentences when you wrote the post?  This 
 
 = 1.0 =
 * Initial release
+
+
+== Upgrade Notice ==
+
+= 1.2 =
+Minor update. Highlights: added filter to customize punctuation that get subsequent double-spacing preserved; added if(function_exists()) check around c2c_extra_sentence_space(); minor text reorganization; added verified WP 3.0 compatibility.
