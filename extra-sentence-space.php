33638a25ccde172f35242b27c29f1d1b76dcce43
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 04:42:21 +0000

    * Update to v1.3.1 * Fix minor error in tests * Minor plugin header reformatting * Minor code reformatting (spacing) * Change documentation links to wp.org to be https * Note compatibility through WP 4.0+ * Add plugin icon
    git-svn-id: https://plugins.svn.wordpress.org/extra-sentence-space/tags/1.3.1@972882 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extra-sentence-space.php
+++ extra-sentence-space.php
@@ -1,71 +1,64 @@
 <?php
 /**
- * @package Extra_Sentence_Space
- * @author Scott Reilly
- * @version 1.1
- */
-/*
-Plugin Name: Extra Sentence Space
-Version: 1.1
-Plugin URI: http://coffee2code.com/wp-plugins/extra-sentence-space
-Author: Scott Reilly
-Author URI: http://coffee2code.com
-Description: Force browsers to display two spaces (when present) between sentences.
-
-Even though you may add two spaces after each sentence when writing a post (assuming you subscribe to a writing style 
-that suggests such spacing) web browsers will collapse consecutive blank spaces into a single space when viewed.
-This plugin adds a &nbsp; (non-breaking space) after sentence-ending punctuation to retain the appearance of your
-two-space intent.
-
-NOTE: The plugin will only enforce the two-space gap in places where two or more spaces actually separate sentences
-in your posts.  It will NOT insert a second space if only one space is present.
-
-Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/extra-sentence-space.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Begin (or continue) to use two spaces to separate your sentences when writing a post.
-
+ * Plugin Name: Extra Sentence Space
+ * Version:     1.3.1
+ * Plugin URI:  http://coffee2code.com/wp-plugins/extra-sentence-space/
+ * Author:      Scott Reilly
+ * Author URI:  http://coffee2code.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Description: Force browsers to display two spaces (when present) between sentences.
+ *
+ * Compatible with WordPress 1.5 through 4.0+.
+ *
+ * =>> Read the accompanying readme.txt file for instructions and documentation.
+ * =>> Also, visit the plugin's homepage for additional information and updates.
+ * =>> Or visit: https://wordpress.org/plugins/extra-sentence-space/
+ *
+* @package Extra_Sentence_Space
+* @author Scott Reilly
+* @version 1.3.1
 */
 
 /*
-Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2008-2014 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions:
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-/**
- * Preserves two spaces (when present) between sentences for display as HTML.
- *
- * The spaces are preserved by the first space being converted to a non-breaking space entity, '&nbsp;'.
- *
- * @param string $text The text to have sentence-ending double-spaces preserved for display as HTML.
- * @return string The converted text
- */
-function c2c_extra_sentence_space( $text ) {
-	return preg_replace("/([\.\!\?][\'\"]?)([ ]{2,})/imsU", "$1&nbsp; ", $text);
-} // end c2c_extra_sentence_space()
+defined( 'ABSPATH' ) or die();
 
-add_filter('comment_text', 'c2c_extra_sentence_space', 9);
-add_filter('the_title', 'c2c_extra_sentence_space', 9);
-add_filter('the_content', 'c2c_extra_sentence_space', 9);
-add_filter('the_excerpt', 'c2c_extra_sentence_space', 9);
-add_filter('widget_text', 'c2c_extra_sentence_space', 9);
+if ( ! function_exists( 'c2c_extra_sentence_space' ) ) :
+	/**
+	 * Preserves two spaces (when present) between sentences for display as HTML.
+	 *
+	 * The spaces are preserved by the first space being converted to a non-breaking space entity, '&nbsp;'.
+	 *
+	 * @param string $text The text to have sentence-ending double-spaces preserved for display as HTML.
+	 * @return string The converted text
+	 */
+	function c2c_extra_sentence_space( $text ) {
+		$punctuation = preg_quote( apply_filters( 'c2c_extra_sentence_space_punctuation', '.!?' ), '/' );
 
-?>
+		return preg_replace( "/([$punctuation][\'\"]?)([ ]{2,})/imsU", "$1&nbsp; ", $text );
+	}
+	add_filter( 'c2c_extra_sentence_space', 'c2c_extra_sentence_space' );
+endif;
+
+add_filter( 'comment_text', 'c2c_extra_sentence_space', 9 );
+add_filter( 'the_title',    'c2c_extra_sentence_space', 9 );
+add_filter( 'the_content',  'c2c_extra_sentence_space', 9 );
+add_filter( 'the_excerpt',  'c2c_extra_sentence_space', 9 );
+add_filter( 'widget_text',  'c2c_extra_sentence_space', 9 );
